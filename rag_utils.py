6a49cc8831f01b2dae5c782d9cb33efba2035148
D-CLOUD-H <1763439652@qq.com>
2026-05-28 18:01:32 +0800

    Create rag_utils.py

---
+++ rag_utils.py
@@ -0,0 +1,7 @@
+            if isinstance(idx, int) and 0 <= idx < len(docs_with_rank):
+                doc = dict(docs_with_rank[idx])
+                score = item.get("relevance_score")
+                if score is not None:
+                    doc["rerank_score"] = score
+                reranked.append(doc)
+          “增量丰富” 而不是 “原地篡改”。
